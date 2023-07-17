a86c9649d5e36b4b64706e495e2cf2922c4f68ac
mitaka00 <mitko.a2000@gmail.com>
2023-07-17 14:50:49 +0300

    Frontend: Header component

--- frontend/app/user/profile.js
+++ frontend/app/user/profile.js
@@ -2,39 +2,24 @@ import { View, Text, ActivityIndicator, TouchableOpacity, SafeAreaView  } from "
 import { Stack, useRootNavigationState } from "expo-router";
 import { useRouter, useSegments } from "expo-router";
 import React from "react";
-import { AuthStore } from "../store";
-import Footer from "./common/footer";
-import Header from "./common/header";
+import Footer from "./../common/footer";
+import Header from "./../common/header";
 
-export default function Home() {
-  const segments = useSegments();
+export default function Profile() {
   const router = useRouter();
-  const { isLoggedIn } = AuthStore.useState((s) => s);
   const navigationState = useRootNavigationState();
-
-  React.useEffect(() => {
-    if (!navigationState?.key) return;
-
-    if (!isLoggedIn ) {
-      router.replace("/auth/login");
-    }
-  }, [isLoggedIn, navigationState?.key]);
 
   return (
     <SafeAreaView  style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Header/>
+        <Header/>
       {!navigationState?.key ? (
         <ActivityIndicator size="large" />
       ) : (
         <View>
-          
-          <View>
-          <Text>Go to Details</Text>
-          <Text>More information</Text>
-          </View>
+            <Text>Profile page</Text>
         </View>
       )}
-      <Footer />
+      <Footer/>
     </SafeAreaView >
   );
 }
--- frontend/constants/icons.js
+++ frontend/constants/icons.js
@@ -1,7 +1,13 @@
 import home from "../assets/icons/house-g27d897c2f_1280.png";
 import addPost from "../assets/icons/plus-g972e5ee8f_1280.png";
+import notification from "../assets/icons/bell-jar-g43f94057f_1280.png";
+import profile from "../assets/icons/blank-profile-picture-g84db2b4e3_1280.png";
+import search from "../assets/icons/magnifying-glass-gf098278b3_1280.png";
 
 export default{
     home,
-    addPost
+    addPost,
+    notification,
+    profile,
+    search,
 };
